71231d16c5c9ff18649cfaa560852e1be39b8f1e
ankit <ak0182274@gmail.com>
2025-08-19 11:42:35 +0530

    refactor: enhance command handling with expiry support for SET and GET commands

--- src/main/java/CommandHandler.java
+++ src/main/java/CommandHandler.java
@@ -3,8 +3,17 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+record ValueWithExpiry(String value, long expiry) {
+    private static final long NO_EXPIRY = -1l;
+
+    public static ValueWithExpiry noExpiry(String val) {
+        return new ValueWithExpiry(val, NO_EXPIRY);
+    }
+}
+
+
 public class CommandHandler {
-    private final Map<String, String> store = new ConcurrentHashMap<>();
+    private final Map<String, ValueWithExpiry> store = new ConcurrentHashMap<>();
 
     // Helper methods
     private ByteBuffer simpleString(String msg) {
@@ -36,15 +45,30 @@ public class CommandHandler {
                 return bulkString(commands[1]);
 
             case "SET":
-                if (commands.length != 3)
+
+                int n = commands.length;
+
+                if (n == 3) {
+                    store.put(commands[1], ValueWithExpiry.noExpiry(commands[2]));
+                } else if (n == 5 && commands[3].equalsIgnoreCase("PX")) {
+                    long expiryTime = Long.parseLong(commands[4]) + System.currentTimeMillis();
+                    store.put(commands[1], new ValueWithExpiry(commands[2], expiryTime));
+                } else {
                     return error("wrong number of arguments for 'set' command");
-                store.put(commands[1], commands[2]);
+                }
                 return simpleString("OK");
 
             case "GET":
-                if (commands.length != 2)
-                    return error("wrong number of arguments for 'get' command");
-                return bulkString(store.get(commands[1]));
+                var record = store.get(commands[1]);
+                if (record == null)
+                    return bulkString(null);
+                var now = System.currentTimeMillis();
+                if (record.expiry() != -1 && now >= record.expiry()) {
+                    store.remove(commands[1]); // remove expired key
+                    return bulkString(null); // return null bulk string
+                }
+
+                return bulkString(record.value());
 
             default:
                 return error("unknown command");
