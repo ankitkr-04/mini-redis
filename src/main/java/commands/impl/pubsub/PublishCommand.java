f7c967ab1b30e32b61cbf758e7334ce89e0de07b
ankit <ak0182274@gmail.com>
2025-08-25 16:41:42 +0530

    fix: implement validation and execution logic in PublishCommand

--- src/main/java/commands/impl/pubsub/PublishCommand.java
+++ src/main/java/commands/impl/pubsub/PublishCommand.java
@@ -3,7 +3,9 @@ package commands.impl.pubsub;
 import commands.base.PubSubCommand;
 import commands.context.CommandContext;
 import commands.result.CommandResult;
+import commands.validation.CommandValidator;
 import commands.validation.ValidationResult;
+import protocol.ResponseBuilder;
 
 public class PublishCommand extends PubSubCommand {
 
@@ -14,14 +16,24 @@ public class PublishCommand extends PubSubCommand {
 
     @Override
     protected ValidationResult performValidation(CommandContext context) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'performValidation'");
+        // PUBLISH <channel> <message>
+        return CommandValidator.validateArgCount(context, 3);
     }
 
     @Override
     protected CommandResult executeInternal(CommandContext context) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'executeInternal'");
-    }
+        String channel = context.getArgs()[1];
+        String message = context.getArgs()[2];
 
+        var pubSubManager = context.getServerContext().getPubSubManager();
+
+        // Gather subscribers *before* publish (to count them accurately)
+        int subscribers = pubSubManager.getAllSubscribers(channel).size();
+
+        // Deliver the message
+        pubSubManager.publish(channel, message);
+
+        // Redis spec: integer reply = number of clients message was delivered to
+        return CommandResult.success(ResponseBuilder.integer(subscribers));
+    }
 }
